8b383d96b364153e57d9736f7f70c9d7c3200c8d
maxime285 <maxou285@gmail.com>
2021-10-25 23:30:14 +0200

    AjoutdeExit

--- MesProgrammes/TestNombrePremier.py
+++ MesProgrammes/TestNombrePremier.py
@@ -1,16 +1,20 @@
 a = "n"
 while a == "n":
-  nbr = input("Rentrez un nombre : ")
-  if nbr == "exit":
-    exit(0)
-  nbr = int(nbr)
-  if nbr > 1:
-    premier = True
-    for i in range(2, int(nbr/2)):
-      if (nbr % i) == 0:
-        print("Ce nombre n'est pas premier")
-        print(i)
-        premier = False
-        break
-    if premier == True:
-      print("Ce nombre est premier")
+  try:
+    nbr = input("Rentrez un nombre : ")
+    nbr = int(nbr) 
+    if nbr > 1:
+      premier = True
+      for i in range(2, int(nbr/2)):
+        if (nbr % i) == 0:
+          print("Ce nombre n'est pas premier")
+          print(i)
+          premier = False
+          break
+      if premier == True:
+        print("Ce nombre est premier")
+  except:
+    if nbr == "exit":
+      exit(0)
+  print("Vous devez rentrer un nombre")
+
