38d256d1e1a3a60e018c3594bd25bba1f5845ce6
maxime285 <maxou285@gmail.com>
2021-10-25 23:09:00 +0200

    AffichePremierDiviseur

--- MesProgrammes/TestNombrePremier.py
+++ MesProgrammes/TestNombrePremier.py
@@ -1,9 +1,16 @@
-nbr = input("Rentrez un nombre : ")
-nbr = int(nbr)
-if nbr > 1:
-  for i in range(2, int(nbr/2)):
-    if (nbr % i) == 0:
-      print("Ce nombre n'est pas premier")
-      break
-  else:
-     print("Ce nombre est premier")
+a = "n"
+while a == "n":
+  nbr = input("Rentrez un nombre : ")
+  if nbr == "exit":
+    exit(0)
+  nbr = int(nbr)
+  if nbr > 1:
+    premier = True
+    for i in range(2, int(nbr/2)):
+      if (nbr % i) == 0:
+        print("Ce nombre n'est pas premier")
+        print(i)
+        premier = False
+        break
+    if premier == True:
+      print("Ce nombre est premier")
