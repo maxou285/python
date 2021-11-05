c7d661e2c3d56df64cfa4a164f08bc82db611e3f
maxime285 <maxou285@gmail.com>
2021-11-05 14:52:48 +0100

    CleanCodeQuestionnaireV3

--- MesProgrammes/ProjetQuestionnaireV3 (Objets).py
+++ MesProgrammes/ProjetQuestionnaireV3 (Objets).py
@@ -1,41 +1,32 @@
 class Question:
-    def __init__(self,question, reponse, nb_question,score):
+    def __init__(self,question, reponse, nb_question):
         self.question = question
         self.reponse = reponse
         self.nb_question = nb_question
-        self.score = score
+
     def poser_question(self):
         print("  QUESTION numero "+ str(self.nb_question+1) + " : " + self.question)
         for i in range(0,4):
             print(str(i+1) + " - " + str(liste_reponses[self.nb_question][i]))
             i += 1
-        self.reponse = input("Votre réponse : ")
+        self.reponse = input("  Votre réponse : ")
+
     def verifier_reponse(self):
         global score
         try:
             if self.reponse == "" :
                 print("ERREUR : Veuillez rentrer une réponse valide ")
                 while self.reponse == "":
-                    i.poser_question()
-                    if i.verifier_reponse():
-                        score = score + 1
+                    Question.verif_except()
                     return
        
             if int(self.reponse) > 4:
                 print("ERREUR : Veuillez rentrer une réponse valide ")
                 while int(self.reponse) > 4:
-                    i.poser_question()
-                    if i.verifier_reponse():
-                        score = score + 1
+                    Question.verif_except()
                     return
         except:
             pass
-                       
-       
-       
-
-
-        
         if  self.reponse.lower() == bonne_reponse[self.nb_question].lower():
             print("Reponse correcte ")
             return True
@@ -47,7 +38,11 @@ class Question:
         return
         
         
-            
+    def verif_except():
+        global score
+        i.poser_question()
+        if i.verifier_reponse():
+            score = score + 1
 
             ### Reposer la question 
 
@@ -57,9 +52,9 @@ score = 0
 nb_totale_questions = 3
 
 
-question1 = Question("Quelle est la capitale de la France ? ","Paris ",0, score)
-question2 = Question("Quelle est la capitale de l'Italie ? ","Rome ",1, score)    
-question3 = Question("Quelle est la capitale de la Belgique ? ","Bruxelles",2, score )       
+question1 = Question("Quelle est la capitale de la France ? ","Paris ",0)
+question2 = Question("Quelle est la capitale de l'Italie ? ","Rome ",1)    
+question3 = Question("Quelle est la capitale de la Belgique ? ","Bruxelles",2)       
 liste_question = [question1, question2, question3]
     #("Quelle est la capitale de la France ?",("Marseille","Nice","Paris","Nantes"),"Paris"),
     #("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"),"Rome")
@@ -71,7 +66,7 @@ liste_reponses = (("Marseille","Nice","Paris","Nantes"),
 
                      
 
-i = 1
+
 
 
 for i in liste_question:
@@ -80,14 +75,14 @@ for i in liste_question:
         score = score + 1
 
 
-print("Score final : " + str(score))
+print(" Score final : " + str(score))
 if score == nb_totale_questions:
-    print("Excellent ! ")
+    print("---Excellent !--- ")
 elif score == 0:
-    print("Revisez votre géographie ")
+    print("---Revisez votre géographie--- ")
 elif score > nb_totale_questions/2:
-    print("Pas mal")
+    print("---Pas mal---")
 elif score < nb_totale_questions/2 and score != 0:
-    print("Peu mieux faire ")
+    print("---Peu mieux faire--- ")
 
     
