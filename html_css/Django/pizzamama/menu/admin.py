cd3b9d78646297b857afdb31b237b62aa90359bf
maxime285 <maxou285@gmail.com>
2021-12-02 10:47:05 +0100

    Adds lots of files 2

---
+++ html_css/Django/pizzamama/menu/admin.py
@@ -0,0 +1,13 @@
+from re import search
+
+from django.contrib import admin
+
+from .models import Pizza
+
+
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'ingrédients', 'vegetarienne', 'prix')
+    search_fields = ['nom']
+# Register your models here.
+
+admin.site.register(Pizza,PizzaAdmin)
