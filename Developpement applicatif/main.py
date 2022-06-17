88bcbe2b9d6fe29af3ae282a01c3f8076badc886
maxime285 <maxou285@gmail.com>
2022-06-17 22:05:34 +0200

    mise à niveau

--- Developpement applicatif/main.py
+++ Developpement applicatif/main.py
@@ -1,6 +1,19 @@
-import kivy
-from kivy.app import App
 
+from kivy.app import App                          #Structuer de bas de kivy 1 class App 1 class MainWidget ou layout et .run
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+class MainWidget(Widget):
+    pass
 
+class BoxLayoutExemple(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text="A")
+        self.add_widget(b1)
+        b2 = Button(text="B")
+        self.add_widget(b2)
 class LeLabApp(App):
-    
+    pass    
+
+LeLabApp().run()
