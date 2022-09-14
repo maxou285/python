673ea99e8ce8643b0fa5ffd6cead3d7db01b308a
maxime285 <maxou285@gmail.com>
2022-09-14 19:21:44 +0200

    PongGame Fix PaddleOpponent

--- MesProgrammes/PongGame/game.py
+++ MesProgrammes/PongGame/game.py
@@ -1,7 +1,7 @@
 
 import pygame
 from paddle import Paddle
-from paddleopponent import PaddleOpponent
+
 from ball import Ball
 # Creer une seconde class qui va representer notre jeu
 class Game:
@@ -9,7 +9,8 @@ class Game:
     def __init__(self):
         # generer les raquettes
         self.paddle = Paddle()
-        self.paddleopponent = PaddleOpponent()
+        self.paddleopponent = Paddle(True)
+        #self.paddleopponent = PaddleOpponent()
         self.ball = Ball()
         self.pressed = {}
        
--- MesProgrammes/PongGame/paddle.py
+++ MesProgrammes/PongGame/paddle.py
@@ -4,13 +4,19 @@ import pygame
 
 
 class Paddle(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, opponent=1):
         super().__init__()
         self.speed = 5
         self.image = pygame.image.load('ressources/PongPaddlesRed.png')
         self.rect = self.image.get_rect()                                       # rect permet de mettre l'image dans un carré et de la déplacer il faut la mettre
         self.rect.x = 50                                           # modifie le point de spawn du sprite(joueur sur l'axe x ) 
         self.rect.y = 300
+        if opponent == True:
+            self.imageopponent = pygame.image.load('ressources/PongPaddlesBlue.png')
+            self.rectopponent = self.imageopponent.get_rect()                                       # rect permet de mettre l'image dans un carré et de la déplacer il faut la mettre
+            self.rectopponent.x = 1015                                           # modifie le point de spawn du sprite(joueur sur l'axe x ) 
+            self.rectopponent.y = 300
+
 
 
     def move_up(self):
@@ -19,3 +25,10 @@ class Paddle(pygame.sprite.Sprite):
     def move_down(self):
         if self.rect.y < (720-148):
             self.rect.y += self.speed
+
+    def move_up_opponent(self):
+        if self.rectopponent.y >= 10:
+            self.rectopponent.y -= self.speed
+    def move_down_opponent(self):
+        if self.rectopponent.y < (720-148):
+            self.rectopponent.y += self.speed
