d76c63aac5c7e2da7c262063493055b4a3783d5f
maxime285 <maxou285@gmail.com>
2022-10-02 15:04:32 +0200

    PizzaMamaApiFinish

--- Developpement Web/PizzaMamaDjango/pizzamama/menu/views.py
+++ Developpement Web/PizzaMamaDjango/pizzamama/menu/views.py
@@ -1,5 +1,7 @@
+from curses.ascii import HT
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
 from .models import Pizza
 
 # Create your views here.
@@ -15,3 +17,8 @@ def index(request):
     return HttpResponse("Les pizzas : " + pizzas_names_and_prices_str)'''
     pizzas = Pizza.objects.all().order_by('prix')
     return render(request, 'menu/index.html', {'pizzas': pizzas})
+
+def api_get_pizzas(request):
+    pizzas = Pizza.objects.all().order_by('prix')
+    json = serializers.serialize("json", pizzas)
+    return HttpResponse(json)
--- Developpement Web/PizzaMamaDjango/pizzamama/pizzamama/urls.py
+++ Developpement Web/PizzaMamaDjango/pizzamama/pizzamama/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu-pizza/', include('menu.urls')),
+    path('api/', include('menu.api_urls')),
     path('', include('main.urls')),
+    
 ]
