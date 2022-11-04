e6181fb75f1b53003e5d5a91aa4c2f5822d7b88a
maxime285 <maxou285@gmail.com>
2022-11-04 21:10:29 +0100

    Save

---
+++ Developpement Web/PizzaMamaDjango-modified/pizzamama/welcomepizza/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WelcomepizzaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'welcomepizza'
--- Developpement Web/PizzaMamaDjango/pizzamama/menu/api_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'menu'
-
-urlpatterns = [
-    path('GetPizzas', views.api_get_pizzas),
-]
